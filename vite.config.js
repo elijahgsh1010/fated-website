f24da10800fba1f8311d9ded5a0f56273aa95b6c
eli_el <elijahgsh@gmail.com>
2024-06-05 14:09:01 +0800

    add server config to test

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,14 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  server: {
+    port: 3000,
+    https: true,
+    hmr: {
+      host: "www.find-a-date.com",
+      port: 3000,
+      protocol: "wss",
+    },
+  },
 })
